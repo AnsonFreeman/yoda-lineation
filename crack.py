2a3c1b236e2843d9088cc70771b6463f8b32dc16
Anson <srkid0223@163.com>
2022-11-03 22:38:22 +0800

    更新翻译

--- crack.py
+++ crack.py
@@ -194,7 +194,7 @@ def crack():
     #聚焦
     pyautogui.moveTo(descLocate[0] / ratio, descLocate[1] / ratio)
     pyautogui.click()
-    pyautogui.moveTo(points['from'][0] / ratio, points['from'][1] / ratio)
+    pyautogui.moveTo(points['from'][0] / ratio+random.randrange(-20, 20, 1), points['from'][1] / ratio+random.randrange(-20, 20, 1))
     pyautogui.click()
     time.sleep(0.5)
 
