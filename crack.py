966783527e85ecf509720610d308e51a65db41cd
Anson <srkid0223@163.com>
2022-11-03 19:25:24 +0800

    更新滑动函数

--- crack.py
+++ crack.py
@@ -75,7 +75,7 @@ def drag(x, y, ratio=2):
     # else:
     #     sec = distance/ ratio / 100
 
-    points = get_points(x, y, distance / 15)
+    points = get_points(x, y, distance / 8)
 
     print('points:',points)
     pyautogui.moveTo(x[0] / ratio, x[1] / ratio, 0.5, pyautogui.easeOutQuad)
@@ -85,7 +85,7 @@ def drag(x, y, ratio=2):
     pyautogui.mouseDown()
 
     for point in points:
-        pyautogui.moveTo(point[0] / ratio, point[1] / ratio, 0.1, pyautogui.easeInOutQuad)
+        pyautogui.moveTo(point[0] / ratio, point[1] / ratio, 0.2, pyautogui.easeInOutQuad)
 
 #    pyautogui.move(y[0]/ratio, y[1]/ratio, 0.2, pyautogui.linear)
     pyautogui.mouseUp()
